f6f65f81761947526234f70286f9888a63c23068
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-09-23 13:51:12 +0400

    feat: Add RoleEntity & ERole

---
+++ src/main/java/com/ryzend/app/entity/ERole.java
@@ -0,0 +1,6 @@
+package com.ryzend.app.entity;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/ryzend/app/entity/RoleEntity.java
@@ -0,0 +1,23 @@
+package com.ryzend.app.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+
+
+}
