354f409bad408abca6fc4e92825a907558a78b29
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-09-23 13:36:17 +0400

    Init commit

---
+++ src/main/java/com/ryzend/app/JwtHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.ryzend.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtHibernateApplication.class, args);
+    }
+
+}
