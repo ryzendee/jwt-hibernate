bc2cc032f9f9294ff0c787c72f9cc5cd9bbc0c39
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-09-24 13:09:22 +0400

    feat: Implement UserDetails in UserEntity

--- src/main/java/com/ryzend/app/entity/UserEntity.java
+++ src/main/java/com/ryzend/app/entity/UserEntity.java
@@ -3,8 +3,12 @@ package com.ryzend.app.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,7 +19,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -34,5 +38,30 @@ public class UserEntity {
     public void addUserRoleEntity(UserRoleEntity userRoleEntity) {
         userRoleEntityList.add(userRoleEntity);
         userRoleEntity.setUser(this);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
     }
 }
